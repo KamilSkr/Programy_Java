adf7d17359f611b063488d09d8f2ba56fd62c7ee
KamilSkr <59320706+KamilSkr@users.noreply.github.com>
2020-05-04 20:54:44 +0200

    Update Przelicznik.java

--- Przelicznik.java
+++ Przelicznik.java
@@ -13,3 +13,25 @@ public class Przelicznik {
         
     }
 }
+///////////////////////////////////////////////
+//Rozrzeszenie o petle 
+//////////////////////////////////////////////////
+import java.util.Scanner;
+public class Przelicznik {
+    public static void main(String[] args) {
+        double metry, jardy, cale, stopy, mile;
+        String d;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Podaj liczbe w metrach: ");
+        metry = sc.nextInt();
+        while(metry != 0){
+        jardy = metry*1.09361;
+        cale = metry*39.3700;
+        stopy = metry*3.2808;
+        mile = metry*0.0006;
+        System.out.println("Metry: " + metry + " \nCale: "+ cale+ "\nJardy: "+jardy+"\nStopy: "+stopy+"\nMile: "+mile);
+        System.out.println("Czy zakonczyc program? wcisnij '0' , Jesli chcesz dalej obliczac podaj liczbe: ");
+        metry = sc.nextInt();
+        }
+    }
+}
